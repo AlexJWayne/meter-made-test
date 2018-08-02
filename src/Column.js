5fd9310cb44defbb8bbf5ace8ea8865e3c5b4f1b
Alex Wayne <alex@beautifulpixel.com>
2018-08-02 12:39:06 -0700

    Trumpet shape!

--- src/Column.js
+++ src/Column.js
@@ -3,10 +3,6 @@ import Meter from "./Meter";
 import "./index.css";
 
 export default class Column extends Component {
-  ledsForMeter(i) {
-    return this.props.leds.slice(i * 4, (i + 1) * 4);
-  }
-
   render() {
     const angle = 180 + this.props.angle * 360;
     return (
@@ -15,7 +11,10 @@ export default class Column extends Component {
         style={
           this.props.is3d
             ? {
-                transform: `rotateY(${angle}deg) translate3d(0, 0, 15em) `
+                transform: `
+                  rotateY(${angle}deg)
+                  translate3d(0, 0, 15.5em)
+                `
               }
             : {}
         }
@@ -23,16 +22,16 @@ export default class Column extends Component {
       >
         {this.props.proximity && <div className="proximity-indicator" />}
         <div className="double-meter">
-          <Meter leds={this.ledsForMeter(0)} />
-          <Meter leds={this.ledsForMeter(1)} />
+          <Meter index={0} leds={this.props.leds} is3d={this.props.is3d} />
+          <Meter index={1} leds={this.props.leds} is3d={this.props.is3d} />
         </div>
-        <Meter leds={this.ledsForMeter(2)} />
-        <Meter leds={this.ledsForMeter(3)} />
-        <Meter leds={this.ledsForMeter(4)} />
-        <Meter leds={this.ledsForMeter(5)} />
-        <Meter leds={this.ledsForMeter(6)} />
-        <Meter leds={this.ledsForMeter(7)} />
-        <Meter leds={this.ledsForMeter(8)} />
+        <Meter index={2} leds={this.props.leds} is3d={this.props.is3d} />
+        <Meter index={3} leds={this.props.leds} is3d={this.props.is3d} />
+        <Meter index={4} leds={this.props.leds} is3d={this.props.is3d} />
+        <Meter index={5} leds={this.props.leds} is3d={this.props.is3d} />
+        <Meter index={6} leds={this.props.leds} is3d={this.props.is3d} />
+        <Meter index={7} leds={this.props.leds} is3d={this.props.is3d} />
+        <Meter index={8} leds={this.props.leds} is3d={this.props.is3d} />
       </div>
     );
   }
--- src/Meter.js
+++ src/Meter.js
@@ -3,23 +3,48 @@ import Led from "./Led";
 import "./index.css";
 
 export default class Meter extends Component {
+  getLeds() {
+    const meter = this.props.index;
+    return this.props.leds.slice(meter * 4, (meter + 1) * 4);
+  }
+
   color() {
+    const leds = this.getLeds();
     const rgb = [
-      (this.props.leds.reduce((total, v) => total + v[0], 0) / 4) * 0.8,
-      (this.props.leds.reduce((total, v) => total + v[1], 0) / 4) * 0.8,
-      (this.props.leds.reduce((total, v) => total + v[2], 0) / 4) * 0.8
+      (leds.reduce((total, v) => total + v[0], 0) / 4) * 0.8,
+      (leds.reduce((total, v) => total + v[1], 0) / 4) * 0.8,
+      (leds.reduce((total, v) => total + v[2], 0) / 4) * 0.8
     ];
 
     return `rgb(${rgb.join(",")})`;
   }
 
   render() {
+    const leds = this.getLeds();
+    const upness = Math.pow(
+      this.props.index <= 1 ? 1 : (8 - (this.props.index - 2)) / 9,
+      2
+    );
+    const angle = upness * -30;
+    const outness = upness * 8 - 8;
     return (
-      <div className="meter" style={{ backgroundColor: this.color() }}>
-        <Led color={this.props.leds[0]} />
-        <Led color={this.props.leds[1]} />
-        <Led color={this.props.leds[2]} />
-        <Led color={this.props.leds[3]} />
+      <div
+        className="meter"
+        style={{
+          backgroundColor: this.color(),
+          transform: this.props.is3d
+            ? `
+                  translate3d(0, 0, ${outness}em)
+                  rotateX(${angle}deg)
+                  scale(0.8, 0.8)
+                `
+            : "none"
+        }}
+      >
+        <Led color={leds[0]} />
+        <Led color={leds[1]} />
+        <Led color={leds[2]} />
+        <Led color={leds[3]} />
       </div>
     );
   }
